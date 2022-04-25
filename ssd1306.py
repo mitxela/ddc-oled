d2c94bafe6d90d564dca53cca952a8ed7b12cabb
mitxela <mitxela@mitxela.com>
2022-04-26 00:03:10 +0100

    combine startup commands into single transaction

--- ssd1306.py
+++ ssd1306.py
@@ -17,38 +17,39 @@ def ssd1306_data(data):
 
 
 # ssd1306 init
-ssd1306_cmd([0xae]) # Display off (reset)
-ssd1306_cmd([0xd5]) # Set display clock div
-ssd1306_cmd([0x80]) #     Suggested ratio (reset: 0x80)
-ssd1306_cmd([0xa8]) # Set mux ratio
-ssd1306_cmd([0x3f]) #     height-1 (reset: 63)
-ssd1306_cmd([0xd3]) # Set display offset
-ssd1306_cmd([0x00]) #     No offset (reset: 0)
-ssd1306_cmd([0x40]) # Set startline = 0 (0x40 | 0x00) (reset: 0x40)
+ssd1306_cmd([
+0xae, # Display off (reset)
+0xd5, # Set display clock div
+0x80, #     Suggested ratio (reset: 0x80)
+0xa8, # Set mux ratio
+0x3f, #     height-1 (reset: 63)
+0xd3, # Set display offset
+0x00, #     No offset (reset: 0)
+0x40, # Set startline = 0 (0x40 | 0x00) (reset: 0x40)
 
-ssd1306_cmd([0x8d]) # Charge pump - documented separately
-ssd1306_cmd([0x14]) #     charge pump enable
+0x8d, # Charge pump - documented separately
+0x14, #     charge pump enable
 
-ssd1306_cmd([0x20]) # Memory mode
-ssd1306_cmd([0x00]) #     Horizontal addressing mode (reset: page address mode)
-ssd1306_cmd([0xa1]) # Segment remap: col 127 is mapped to SEG0
+0x20, # Memory mode
+0x00, #     Horizontal addressing mode (reset: page address mode)
+0xa1, # Segment remap: col 127 is mapped to SEG0
 
-ssd1306_cmd([0xc8]) # com scan direction remapped (reset: 0xc0)
+0xc8, # com scan direction remapped (reset: 0xc0)
 
-ssd1306_cmd([0xda]) # Set com pins
-ssd1306_cmd([0x12]) #     reset: 0x12
+0xda, # Set com pins
+0x12, #     reset: 0x12
 
-ssd1306_cmd([0x81]) # Set contrast
-ssd1306_cmd([0xcf]) #     Contrast for internal vcc (reset: 0x7f)
+0x81, # Set contrast
+0x0f, #     Contrast for internal vcc (reset: 0x7f)
 
-ssd1306_cmd([0xd9]) # Set precharge period
-ssd1306_cmd([0xf1]) #     for internal vcc (reset: 0x22)
-ssd1306_cmd([0xdb]) # Set vcomh deselect level
-ssd1306_cmd([0x40]) #     undocumented (reset: 0x20)
+0xd9, # Set precharge period
+0xf1, #     for internal vcc (reset: 0x22)
+0xdb, # Set vcomh deselect level
+0x40, #     undocumented (reset: 0x20)
 
-ssd1306_cmd([0xa4]) # Resume entire display on (reset)
-ssd1306_cmd([0xa6]) # Normal display (reset)
-ssd1306_cmd([0xaf]) # Display on
+0xa4, # Resume entire display on (reset)
+0xa6, # Normal display (reset)
+0xaf]) # Display on
 
 
 from Xlib import display, X
